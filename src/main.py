fe895153cc8c523ef5182e0d3a9d24f49e9b9680
Guhirwa <christianguhirwa74@gmail.com>
2026-04-23 20:14:17 +0200

    feat(main): implementing utilizing the find_item function
    - prepared the two element to receive the returned values from the find_item function
    - passed the arguments as required
    	1. the list of dictionaries (inventory)
    	2. Callable /exutable argument which is an anonymous / lambda function
    - at this time the idx return will not be used
    - cb in the find_item will receive that lambda function and the when called by cb(item) in the loop it will use the id of the current item to return the boolean value directily id the condition become true. So will exit the loop as well as the function

--- src/main.py
+++ src/main.py
@@ -1,14 +1,14 @@
-from pydoc import describe
-from unittest.mock import Base
-
+'''Entry point of the system'''
 from typing import Optional
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel, Field
-
-from sqlalchemy import desc
+from utils import find_item
 
 fastapi = FastAPI()
 
+inventory = [
+    {'id': 1, 'name': 'Treasure', 'quantity': 3}
+]
 
 class Item(BaseModel):
     """Pydantic model for item structure and validation"""
@@ -22,3 +22,19 @@ class ItemUpdate(BaseModel):
 
     name: Optional[str] = Field(None, description="Optional name for an Item")
     quantity: Optional[int] = Field(None, description="Optional quantity of an Item")
+
+@fastapi.get('/{username}')
+async def home(username: str):
+    '''Home route handler to welcome the user wih a greet message'''
+    return {'message': f'Hello {username}, Welcome !!!'}
+
+@fastapi.get('/items')
+async def get_items():
+    '''Route handler for get all items'''
+    return {'items': inventory}
+
+@fastapi.get('/items/{item_id}')
+async def get_item(item_id: int):
+    '''Route handler for calling the searching functionality'''
+    item, idx = find_item(inventory, lambda x: x['id'] == item_id)
+    return {'item': item}
